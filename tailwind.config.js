5de5538c331e1f3fb0906571f9af3c934b1d7869
tapiwa-1 <tmotsi10@gmail.com>
2023-06-07 16:16:29 +0200

    fixing bugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,36 +10,8 @@ module.exports = {
         "./node_modules/flowbite.{js,ts}"
     ],
     darkMode: 'class',
-    theme: {
 
-        fontFamily: {
-            sans: ['Graphik', 'sans-serif'],
-            serif: ['Merriweather', 'serif'],
-        },
-        screens: {
-            'sm': '640px',
-            'md': '768px',
-            'lg': '1024px',
-            'xl': '1280px',
-            '2xl': '1536px',
-        },
-        extend: {
-            spacing: {
-                '128': '32rem',
-                '144': '36rem',
-            },
-            borderRadius: {
-                '4xl': '2rem',
-            }
-        },
-    },
-    variants: {
-        fill: [],
-        extend: {
-            borderColor: ['focus-visible'],
-            opacity: ['disabled'],
-        }
-    },
+
     plugins: [
         require('flowbite/plugin')
     ],
